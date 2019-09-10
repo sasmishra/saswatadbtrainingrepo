a083a2fa51cd9884d37b65df35dbf8f5744042d4
sasmishra <54226754+sasmishra@users.noreply.github.com>
2019-09-10 12:53:26 +0530

    Update: README.md notebooks

---
+++ notebooks/SaswatTrainingPractice1.scala
@@ -0,0 +1,9 @@
+// Databricks notebook source
+
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC 
+// MAGIC Learning ADB Proffessionally
+// MAGIC This notebook would configure ADB Volumn Mounting with DLS Gen 2
